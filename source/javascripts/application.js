5e57094cf984411c14a05243cfc22796eebce3b3
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-01-09 18:41:54 +0000

    Force SSL with JS hack

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,6 +1,7 @@
 // This is where it all goes :)
 
 // Force HTTPS hack - needs to happen at server
+// UNLESS LOCALHOST!!!
 if (location.protocol !== 'https:') {
     location.replace(`https:${location.href.substring(location.protocol.length)}`);
 }
