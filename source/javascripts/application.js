ef8791f6b7e97a9d0c4b61bc9d34cac614c8870e
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-01-10 10:11:21 +0000

    Open Graph meta tags for social media

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,11 +1,4 @@
-// This is where it all goes :)
-
-console.log('Running application.js...');
-
-// Force HTTPS hack - needs to happen at server
-// UNLESS LOCALHOST!!!
-
+// Force HTTPS hack - should happen at DNS level?
 if (location.host !== "localhost:4567" && location.protocol !== 'https:') {
-    console.log('Replacing location.protocol...');
     location.protocol = "https:";
 }
