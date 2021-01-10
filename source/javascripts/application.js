3ca8a60ea91a4e46fa15cd0a9685418d21e4d26a
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-01-10 09:54:27 +0000

    Force SSL with JS

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,7 +1,12 @@
 // This is where it all goes :)
 
+console.log('Running application.js...');
+
 // Force HTTPS hack - needs to happen at server
 // UNLESS LOCALHOST!!!
-if (location.protocol !== 'https:') {
-    location.replace(`https:${location.href.substring(location.protocol.length)}`);
+
+if (location.host !== "localhost:4567" && location.protocol !== 'https:') {
+    console.log('Replacing location.protocol...');
+    location.protocol = "https:";
+    console.log(`Updated location.protocol:${location.protocol}`);
 }
